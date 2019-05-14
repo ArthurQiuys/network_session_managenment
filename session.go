fe9df20f42fb567c1bf59ca06e1ef6864e5dd1c9
qiuyuanshun <qiuyuanshun@yeah.net>
2019-05-15 00:30:08 +0800

    about session

--- session.go
+++ session.go
@@ -2,7 +2,7 @@
 * @Author: qiuyu
 * @Date:   2019-05-08 06:15:04
 * @Last Modified by:   qiuyu
-* @Last Modified time: 2019-05-08 06:27:36
+* @Last Modified time: 2019-05-15 00:29:53
 */
 package network_session_managenment
 
@@ -37,15 +37,29 @@ type closeCallback struct {
 	Next    *closeCallback
 }
 
-func NewSession(codec *Manger, codec Codec, sendChanSize int) *Session {
-	
+func NewSession(codec Codec, sendChanSize int) *Session {
+	return NewSession(nil, codec, sendChanSize)
+}
+func newSession(manager *Manger, codec Codec, sendChanSize int) *Session{
+		seesion := &Session{
+		codec: codec,
+		manager: manager,
+		closeChan: make(chan int),
+		id: atomic.AddUint64(&globalSessionId, 1),
+	}
+
+	if sendChanSize > 0{
+		seesion.sendChan = make(chan interface{}, sendChanSize)
+		go seesion.sendLoop()
+	}
+	return seesion
 }
 func (session *Session)ID()uint64 {
-	
+	return seesion.id
 }
 
 func (seesion *Session)IsClosed() bool {
-	
+	return atomic.LoadInt32(&seesion.closeFlag, 0, 1)
 }
 
 func (seesion *Session)Close() error{
